a86cc0abcf97db4e23e15fb6c1150f06067dabaa
jade78589 <cosasmundoxat25@hotmail.com>
2025-08-14 23:04:26 -0500

    Update repro3.js

--- js/repro3.js
+++ js/repro3.js
@@ -496,6 +496,7 @@ return j.call(r(a),c)})),b))for(;i>h;h++)b(a[h],c,g?d:d.call(a[h],h,b(a[h],c)));
 
 
               $("#stream1").icast({
-          	     URL: "https://technoplayerserver.net:8200",
+          	     URL:  "https://antyserv.in/8100",
           		version: "2",
+
               })
